0daea4ee7d0df97dee3dc3a69f9581b7fcd2d587
sabrinaliu <sliu.owl68@yahoo.com>
2018-09-15 22:06:39 -0400

    Add gitignore and blob endpoint

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const FormData = requre('form-data')
 const app = express()
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/postAudioFile', function(req, res) {
-	revPOST(req.body).then((response) => res.send(response))
+app.post('/sendAsMediaLink', function(req, res) {
+	revPOSTWithJSON(req.body).then((response) => res.send(response))
+})
+
+app.post('/sendAsBlob', function(req, res) {
+	revPOSTWithForm(req.body).then(response => res.send(response))
 })
 
 app.get('/getTranscript/:transcriptId', function(req, res) {
@@ -17,7 +22,30 @@ app.get('/getTranscript/:transcriptId', function(req, res) {
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-function revPOST(data) {
+function revPOSTWithForm(data){
+	var config = {
+		headers: {
+			'Authorization' : 'Bearer 0146doeT5wCQ-_Bk_LirlfoVya1pIBDWVCMbqjV77TMFWixEt4kOWQkAojkHER2q3wM6Cvvk-MnWhffLjgZG-zupFIIKs',
+			'Content-Type' : 'multipart/form-data'
+		}
+	};
+
+	const form = new FormData();
+	form.append('media', data);
+	form.append('metadata', "This is a sample submit jobs option for multipart")
+
+	console.log(form.getHeaders())
+
+	return axios.post('https://api.rev.ai/revspeech/v1beta/jobs', form, config)
+		.then(response => {
+			return response.data
+		}).catch(error => {
+			return error.data
+		})
+
+}
+
+function revPOSTWithJSON(data) {
 	var config  = {
 		headers: {
 			'Authorization' : 'Bearer 0146doeT5wCQ-_Bk_LirlfoVya1pIBDWVCMbqjV77TMFWixEt4kOWQkAojkHER2q3wM6Cvvk-MnWhffLjgZG-zupFIIKs',
